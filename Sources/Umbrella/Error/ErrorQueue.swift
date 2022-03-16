47c3962dc926073d524cf5e27b59db35d4dd472f
jeffreybergier <jeffburg@jeffburg.com>
2022-03-16 17:21:29 +0900

    added more useful environment queue

--- Sources/Umbrella/Error/ErrorQueue.swift
+++ Sources/Umbrella/Error/ErrorQueue.swift
@@ -1,5 +1,5 @@
 //
-//  Created by Jeffrey Bergier on 2022/01/08.
+//  Created by Jeffrey Bergier on 2022/03/13.
 //
 //  MIT License
 //
@@ -11,9 +11,10 @@
 //  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 //  copies of the Software, and to permit persons to whom the Software is
 //  furnished to do so, subject to the following conditions:
+//
 //  The above copyright notice and this permission notice shall be included in all
 //  copies or substantial portions of the Software.
-
+//
 //  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 //  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 //  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,19 +27,54 @@
 import SwiftUI
 import Collections
 
+public typealias ErrorQueue = EnvironmentQueue<UserFacingError>
+
+/// Makes it easy to add a queue of errors to your application. Add a `Environment`
+/// into your environment at the top level of your application. Then use this property wrapper
+/// so that any view can access the error queue. If the property is NIL there are no errors in the
+/// queue. If the property contains an error, it should be presented to the user. This is very easy
+/// using `.modifier(UserFacingErrorAlert(self.$errorQueue))` syntax for presenting
+/// errors. When the error is dismissed, the system sets the property to NIL. In this case, the error
+/// is removed the queue and the next error is presented.
 @propertyWrapper
 public struct EnvironmentQueue<T>: DynamicProperty {
-    public static func environmentObject() -> BlackBox<Deque<T>> {
-        BlackBox<Deque<T>>([])
+    
+    public typealias Environment = BlackBox<Collection>
+    public typealias Collection = Deque<T>
+    
+    public static func newEnvirementObject() -> Environment {
+        return BlackBox(Deque<T>(), isObservingValue: true)
     }
-    @EnvironmentObject private var store: BlackBox<Deque<T>>
+    
+    @EnvironmentObject public var environment: Environment
+    
     public init() {}
-    public var wrappedValue: Deque<T> {
-        get { self.store.value }
-        nonmutating set { self.store.value = newValue }
+    
+    /// Contains the next error in the Queue. Set to a new error to append a new error to the end of the
+    /// queue. Set to NIL to remove the first error from the queue. Directly access `environment`
+    /// property if you wish to manually modify the queue.
+    public var wrappedValue: T? {
+        get {
+            return self.environment.value.first
+        }
+        nonmutating set {
+            if let newValue = newValue {
+                self.environment.value.append(newValue)
+            } else {
+                guard self.environment.value.isEmpty == false else {
+                    "Tried to remove error from queue when it was already empty".log()
+                    return
+                }
+                self.environment.value.removeFirst()
+            }
+        }
     }
-    public var projectedValue: Binding<Deque<T>> {
-        Binding { self.wrappedValue }
-        set: { self.store.value = $0 }
+    
+    public var projectedValue: Binding<T?> {
+        Binding {
+            self.wrappedValue
+        } set: {
+            self.wrappedValue = $0
+        }
     }
 }
