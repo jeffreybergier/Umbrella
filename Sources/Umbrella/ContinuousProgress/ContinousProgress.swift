19056f4c9ac2d708cbd4cd79e0d13afd1b904548
jeffreybergier <jeffburg@jeffburg.com>
2023-01-20 11:48:24 +0900

    [errors] attempting to simplify continuous progress

--- Sources/Umbrella/ContinuousProgress/ContinousProgress.swift
+++ Sources/Umbrella/ContinuousProgress/ContinousProgress.swift
@@ -27,71 +27,27 @@
 import Combine
 import CoreData
 
-/// Represents the progress of something that is long running and can produce
-/// errors upon startup and errors while running. I use this to reflect the status
-/// of NSPersistentCloudKitContainer sync. But its generic enough to be used for
-/// many types of long-running / background processes.
-/// Use `AnyContinousProgress` to get around AssociatedType compile errors.
-public protocol ContinousProgress: ObservableObject {
-    associatedtype Error: Swift.Error
-    var progress: Progress { get }
-    /// When an error occurs, append it to the Queue.
-    var errors: [Error] { get set }
-}
-
-public enum CPError: CustomNSError {
-    case accountStatus(CPAccountStatus)
-    case sync(NSError?)
+public struct ContinousProgress: Identifiable {
+    /// Because Error and Progress are not Hashable, Equatable,
+    /// use ID to tell if something changed
+    public var id: UUID = .init()
     
-    public var errorCode: Int {
-        switch self {
-        case .accountStatus:
-            return 1001
-        case .sync:
-            return 1002
+    /// Progress of any operations
+    public var progress: Progress {
+        didSet {
+            self.id = .init()
         }
     }
     
-    public static var errorDomain: String {
-        "com.saturdayapps.Umbrella.ContinuousProgressError"
-    }
-}
-
-public class AnyContinousProgress<Error: Swift.Error>: ContinousProgress {
-    
-    public let objectWillChange: ObservableObjectPublisher
-    public var progress: Progress { _progress() }
-    public var errors: [Error] {
-        get { _errors_get() }
-        set { _errors_set(newValue) }
+    /// When an error occurs, append it to the Queue.
+    public var errors: [Swift.Error] {
+        didSet {
+            self.id = .init()
+        }
     }
     
-    private var _progress: () -> Progress
-    private var _errors_get: () -> [Error]
-    private var _errors_set: ([Error]) -> Void
-    
-    public init<T: ContinousProgress>(_ progress: T) where T.Error == Error,
-                T.ObjectWillChangePublisher == ObservableObjectPublisher
-     {
-        self.objectWillChange = progress.objectWillChange
-        _progress             = { progress.progress }
-        _errors_get           = { progress.errors }
-        _errors_set           = { progress.errors = $0 }
+    public init(progress: Progress = .init(), errors: [Error] = []) {
+        self.progress = progress
+        self.errors = errors
     }
-}
-
-/// Use when you have no progress to report
-public class NoContinousProgress<Error: Swift.Error>: ContinousProgress {
-    public let initializeError: Error? = nil
-    public let progress: Progress = .init()
-    public var errors: [Error] = .init()
-    public init() {}
-}
-
-public enum CPAccountStatus: Int {
-    case couldNotDetermine = 0
-    case available = 1
-    case restricted = 2
-    case noAccount = 3
-    case temporarilyUnavailable = 4
 }
